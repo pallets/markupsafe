6b676ac6ccc6ca9fc0006d758d4eb98ee437061e
Armin Ronacher <armin.ronacher@active-4.com>
2011-02-17 23:15:28 +0100

    master is 0.13-dev

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def run_setup(with_binary):
         features['speedups'] = speedups
     setup(
         name='MarkupSafe',
-        version='0.12',
+        version='0.13',
         url='http://dev.pocoo.org/',
         license='BSD',
         author='Armin Ronacher',
