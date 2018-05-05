b159ba30c747859419e25f3570844b5bf8c67eb3
David Lord <davidism@gmail.com>
2018-05-04 19:22:48 -0700

    fix tox typo

--- tests/test_escape.py
+++ tests/test_escape.py
@@ -8,12 +8,6 @@ try:
 except ImportError:
     _speedups = None
 
-
-@pytest.mark.parametrize('mod', (
-    _native,
-    pytest.param(_speedups, marks=pytest.mark.skipif(
-        _speedups is None, reason='speedups unavailable')),
-))
 @pytest.mark.parametrize(('value', 'expect'), (
     # empty
     (u'', u''),
@@ -30,6 +24,11 @@ except ImportError:
     (u'\U0001F363\U0001F362&><\'"\U0001F37A xyz', u'\U0001F363\U0001F362&amp;&gt;&lt;&#39;&#34;\U0001F37A xyz'),
     (u'&><\'"\U0001F37A xyz', u'&amp;&gt;&lt;&#39;&#34;\U0001F37A xyz'),
     (u'\U0001F363\U0001F362&><\'"', u'\U0001F363\U0001F362&amp;&gt;&lt;&#39;&#34;'),
+))
+@pytest.mark.parametrize('mod', (
+    _native,
+    pytest.param(_speedups, marks=pytest.mark.skipif(
+        _speedups is None, reason='speedups unavailable')),
 ))
 def test_escape(mod, value, expect):
     assert mod.escape(value) == Markup(expect)
