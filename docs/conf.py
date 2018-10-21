8c6f43dd17f6f7cefa3768f8b5db277490b4256e
David Lord <davidism@gmail.com>
2018-10-20 20:11:14 -0700

    fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -1,72 +1,42 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-
-from pallets_sphinx_themes import ProjectLink, get_version
+from pallets_sphinx_themes import get_version
+from pallets_sphinx_themes import ProjectLink
 
 # Project --------------------------------------------------------------
 
-project = 'MarkupSafe'
-copyright = '2010 Pallets team'
-author = 'Pallets team'
-release, version = get_version('MarkupSafe')
+project = "MarkupSafe"
+copyright = "2010 Pallets Team"
+author = "Pallets Team"
+release, version = get_version("MarkupSafe")
 
 # General --------------------------------------------------------------
 
-master_doc = 'index'
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-}
+master_doc = "index"
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "pallets_sphinx_themes"]
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
 
 # HTML -----------------------------------------------------------------
 
-html_theme = 'flask'
+html_theme = "flask"
+html_theme_options = {"index_sidebar_logo": False}
 html_context = {
-    'project_links': [
-        ProjectLink(
-            'Donate to Pallets',
-            'https://psfmember.org/civicrm/contribute/transact?reset=1&id=20'),
-        ProjectLink(
-            'MarkupSafe Website', 'https://palletsprojects.com/p/markupsafe/'),
-        ProjectLink('PyPI Releases', 'https://pypi.org/project/MarkupSafe/'),
-        ProjectLink('Source Code', 'https://github.com/pallets/markupsafe/'),
-        ProjectLink(
-            'Issue Tracker', 'https://github.com/pallets/MarkupSafe/issues/'),
-    ],
-}
-html_sidebars = {
-    'index': [
-        'project.html',
-        'searchbox.html',
-    ],
-    '**': [
-        'localtoc.html',
-        'relations.html',
-        'searchbox.html',
+    "project_links": [
+        ProjectLink("Donate to Pallets", "https://palletsprojects.com/donate"),
+        ProjectLink("Website", "https://palletsprojects.com/p/MarkupSafe/"),
+        ProjectLink("PyPI releases", "https://pypi.org/project/MarkupSafe/"),
+        ProjectLink("Source Code", "https://github.com/pallets/markupsafe/"),
+        ProjectLink("Issue Tracker", "https://github.com/pallets/markupsafe/issues/"),
     ]
 }
+html_sidebars = {
+    "index": ["project.html", "localtoc.html", "searchbox.html"],
+    "**": ["localtoc.html", "relations.html", "searchbox.html"],
+}
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
+html_title = "MarkupSafe Documentation ({})".format(version)
 html_show_sourcelink = False
 
 # LaTeX ----------------------------------------------------------------
 
 latex_documents = [
-    (
-        master_doc, 'MarkupSafe.tex', 'MarkupSafe Documentation',
-        'Pallets team', 'manual'
-    ),
+    (master_doc, "MarkupSafe-{}.tex".format(version), html_title, author, "manual")
 ]
-latex_use_modindex = False
-latex_elements = {
-    'papersize': 'a4paper',
-    'pointsize': '12pt',
-}
-latex_use_parts = True
-
-# linkcheck ------------------------------------------------------------
-
-linkcheck_anchors = False
--- docs/make.bat
+++ docs/make.bat
@@ -9,7 +9,6 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=.
 set BUILDDIR=_build
-set SPHINXPROJ=MarkupSafe
 
 if "%1" == "" goto help
 
