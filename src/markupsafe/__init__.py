496112e00fcfa54d81d256f1f7e221ad01d033cc
David Lord <davidism@gmail.com>
2023-06-02 13:16:08 -0700

    release version 2.1.3

--- src/markupsafe/__init__.py
+++ src/markupsafe/__init__.py
@@ -14,7 +14,7 @@ if t.TYPE_CHECKING:
     _P = te.ParamSpec("_P")
 
 
-__version__ = "2.1.3.dev"
+__version__ = "2.1.3"
 
 _strip_comments_re = re.compile(r"<!--.*?-->", re.DOTALL)
 _strip_tags_re = re.compile(r"<.*?>", re.DOTALL)
