73e6a4886564a554c4a19983d29c97f9fc95457d
David Lord <davidism@gmail.com>
2024-02-02 07:31:03 -0800

    start version 2.1.5

--- src/markupsafe/__init__.py
+++ src/markupsafe/__init__.py
@@ -13,7 +13,7 @@ if t.TYPE_CHECKING:
     _P = te.ParamSpec("_P")
 
 
-__version__ = "2.1.4"
+__version__ = "2.1.5.dev"
 
 
 def _simple_escaping_wrapper(func: "t.Callable[_P, str]") -> "t.Callable[_P, Markup]":
