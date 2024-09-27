83755e03978b67a56990ac3dafb2bff1f9c88a0a
Jeff Dairiki <dairiki@dairiki.org>
2024-09-27 07:36:47 -0700

    style: use tabs for consistency
    Co-authored-by: Adam Turner <9087854+AA-Turner@users.noreply.github.com>

--- src/markupsafe/_speedups.c
+++ src/markupsafe/_speedups.c
@@ -191,11 +191,11 @@ PyMODINIT_FUNC
 PyInit__speedups(void)
 {
 	PyObject *m = PyModule_Create(&module_definition);
-    if (m == NULL) {
-        return NULL;
-    }
+	if (m == NULL) {
+		return NULL;
+	}
 #ifdef Py_GIL_DISABLED
-    PyUnstable_Module_SetGIL(m, Py_MOD_GIL_NOT_USED);
+	PyUnstable_Module_SetGIL(m, Py_MOD_GIL_NOT_USED);
 #endif
-    return m;
+	return m;
 }
